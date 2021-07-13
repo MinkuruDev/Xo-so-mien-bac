ba98da8b2616c661a76eab2295a49e35b198fc25
Mars <52197560+MinkuruDev@users.noreply.github.com>
2021-07-13 10:12:16 +0700

    69% done app

--- GuessVietNamNorthenLottery/frmMain.Designer.cs
+++ GuessVietNamNorthenLottery/frmMain.Designer.cs
@@ -42,8 +42,15 @@ namespace GuessVietNamNorthenLottery
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
+            this.lblSpecial = new System.Windows.Forms.Label();
             this.btnGuess = new System.Windows.Forms.Button();
+            this.lblG1 = new System.Windows.Forms.Label();
+            this.lblG2 = new System.Windows.Forms.Label();
+            this.lblG3 = new System.Windows.Forms.Label();
+            this.lblG4 = new System.Windows.Forms.Label();
+            this.lblG5 = new System.Windows.Forms.Label();
+            this.lblG6 = new System.Windows.Forms.Label();
+            this.lblG7 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -69,9 +76,9 @@ namespace GuessVietNamNorthenLottery
             this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label8.Location = new System.Drawing.Point(4, 210);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(60, 28);
+            this.label8.Size = new System.Drawing.Size(64, 28);
             this.label8.TabIndex = 7;
-            this.label8.Text = "Giải 1";
+            this.label8.Text = "Giải 4";
             // 
             // label5
             // 
@@ -79,9 +86,9 @@ namespace GuessVietNamNorthenLottery
             this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label5.Location = new System.Drawing.Point(4, 138);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 28);
+            this.label5.Size = new System.Drawing.Size(63, 28);
             this.label5.TabIndex = 4;
-            this.label5.Text = "Giải 1";
+            this.label5.Text = "Giải 3";
             // 
             // label6
             // 
@@ -89,9 +96,9 @@ namespace GuessVietNamNorthenLottery
             this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label6.Location = new System.Drawing.Point(4, 101);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 28);
+            this.label6.Size = new System.Drawing.Size(63, 28);
             this.label6.TabIndex = 5;
-            this.label6.Text = "Giải 1";
+            this.label6.Text = "Giải 2";
             // 
             // label3
             // 
@@ -129,11 +136,11 @@ namespace GuessVietNamNorthenLottery
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(4, 286);
+            this.label4.Location = new System.Drawing.Point(4, 341);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 28);
+            this.label4.Size = new System.Drawing.Size(63, 28);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Giải 1";
+            this.label4.Text = "Giải 6";
             // 
             // label7
             // 
@@ -141,9 +148,9 @@ namespace GuessVietNamNorthenLottery
             this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label7.Location = new System.Drawing.Point(4, 249);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(60, 28);
+            this.label7.Size = new System.Drawing.Size(63, 28);
             this.label7.TabIndex = 6;
-            this.label7.Text = "Giải 1";
+            this.label7.Text = "Giải 5";
             // 
             // tableLayoutPanel1
             // 
@@ -151,6 +158,12 @@ namespace GuessVietNamNorthenLottery
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.17593F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.82407F));
+            this.tableLayoutPanel1.Controls.Add(this.lblG7, 1, 8);
+            this.tableLayoutPanel1.Controls.Add(this.lblG6, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.lblG5, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.lblG4, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.lblG3, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.lblG2, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.label9, 0, 8);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
@@ -161,7 +174,8 @@ namespace GuessVietNamNorthenLottery
             this.tableLayoutPanel1.Controls.Add(this.label7, 0, 6);
             this.tableLayoutPanel1.Controls.Add(this.label4, 0, 7);
             this.tableLayoutPanel1.Controls.Add(this.label10, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label11, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.lblSpecial, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblG1, 1, 2);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(18, 45);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 9;
@@ -171,9 +185,9 @@ namespace GuessVietNamNorthenLottery
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 71F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 88F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 91F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(433, 418);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
@@ -181,11 +195,11 @@ namespace GuessVietNamNorthenLottery
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label9.Location = new System.Drawing.Point(4, 328);
+            this.label9.Location = new System.Drawing.Point(4, 377);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(60, 28);
+            this.label9.Size = new System.Drawing.Size(63, 28);
             this.label9.TabIndex = 8;
-            this.label9.Text = "Giải 1";
+            this.label9.Text = "Giải 7";
             // 
             // label10
             // 
@@ -193,20 +207,23 @@ namespace GuessVietNamNorthenLottery
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label10.ForeColor = System.Drawing.Color.Blue;
-            this.label10.Location = new System.Drawing.Point(180, 3);
+            this.label10.Location = new System.Drawing.Point(277, 3);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(212, 28);
+            this.label10.Size = new System.Drawing.Size(18, 28);
             this.label10.TabIndex = 9;
-            this.label10.Text = "asd ds ws fse dsx sdw";
+            this.label10.Text = " ";
             // 
-            // label11
+            // lblSpecial
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(143, 138);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(58, 20);
-            this.label11.TabIndex = 10;
-            this.label11.Text = "label11";
+            this.lblSpecial.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblSpecial.AutoSize = true;
+            this.lblSpecial.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSpecial.ForeColor = System.Drawing.Color.Red;
+            this.lblSpecial.Location = new System.Drawing.Point(250, 36);
+            this.lblSpecial.Name = "lblSpecial";
+            this.lblSpecial.Size = new System.Drawing.Size(72, 28);
+            this.lblSpecial.TabIndex = 10;
+            this.lblSpecial.Text = "69696";
             // 
             // btnGuess
             // 
@@ -217,6 +234,83 @@ namespace GuessVietNamNorthenLottery
             this.btnGuess.Text = "Dự Đoán";
             this.btnGuess.UseVisualStyleBackColor = true;
             this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblG1
+            // 
+            this.lblG1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblG1.AutoSize = true;
+            this.lblG1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblG1.Location = new System.Drawing.Point(258, 72);
+            this.lblG1.Name = "lblG1";
+            this.lblG1.Size = new System.Drawing.Size(55, 23);
+            this.lblG1.TabIndex = 11;
+            this.lblG1.Text = "99699";
+            // 
+            // lblG2
+            // 
+            this.lblG2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblG2.AutoSize = true;
+            this.lblG2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblG2.Location = new System.Drawing.Point(228, 107);
+            this.lblG2.Name = "lblG2";
+            this.lblG2.Size = new System.Drawing.Size(115, 23);
+            this.lblG2.TabIndex = 12;
+            this.lblG2.Text = "96966   99696";
+            // 
+            // lblG3
+            // 
+            this.lblG3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblG3.AutoSize = true;
+            this.lblG3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblG3.Location = new System.Drawing.Point(198, 150);
+            this.lblG3.Name = "lblG3";
+            this.lblG3.Size = new System.Drawing.Size(176, 46);
+            this.lblG3.TabIndex = 13;
+            this.lblG3.Text = "12345   34523   12312\r\n34211   23445   23424\r\n";
+            // 
+            // lblG4
+            // 
+            this.lblG4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblG4.AutoSize = true;
+            this.lblG4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblG4.Location = new System.Drawing.Point(186, 217);
+            this.lblG4.Name = "lblG4";
+            this.lblG4.Size = new System.Drawing.Size(200, 23);
+            this.lblG4.TabIndex = 14;
+            this.lblG4.Text = "1234   2345   7382   4235";
+            // 
+            // lblG5
+            // 
+            this.lblG5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblG5.AutoSize = true;
+            this.lblG5.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblG5.Location = new System.Drawing.Point(210, 271);
+            this.lblG5.Name = "lblG5";
+            this.lblG5.Size = new System.Drawing.Size(152, 46);
+            this.lblG5.TabIndex = 15;
+            this.lblG5.Text = "4533   3453   3345\r\n6544   2342   8934";
+            // 
+            // lblG6
+            // 
+            this.lblG6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblG6.AutoSize = true;
+            this.lblG6.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblG6.Location = new System.Drawing.Point(226, 347);
+            this.lblG6.Name = "lblG6";
+            this.lblG6.Size = new System.Drawing.Size(120, 23);
+            this.lblG6.TabIndex = 16;
+            this.lblG6.Text = "123   456   789";
+            // 
+            // lblG7
+            // 
+            this.lblG7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblG7.AutoSize = true;
+            this.lblG7.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblG7.Location = new System.Drawing.Point(222, 385);
+            this.lblG7.Name = "lblG7";
+            this.lblG7.Size = new System.Drawing.Size(127, 23);
+            this.lblG7.TabIndex = 17;
+            this.lblG7.Text = "69   96   06   06";
             // 
             // frmMain
             // 
@@ -253,7 +347,14 @@ namespace GuessVietNamNorthenLottery
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button btnGuess;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblSpecial;
+        private System.Windows.Forms.Label lblG7;
+        private System.Windows.Forms.Label lblG6;
+        private System.Windows.Forms.Label lblG5;
+        private System.Windows.Forms.Label lblG4;
+        private System.Windows.Forms.Label lblG3;
+        private System.Windows.Forms.Label lblG2;
+        private System.Windows.Forms.Label lblG1;
     }
 }
 
--- GuessVietNamNorthenLottery/frmMain.cs
+++ GuessVietNamNorthenLottery/frmMain.cs
@@ -17,11 +17,10 @@ namespace GuessVietNamNorthenLottery
             InitializeComponent();
         }
 
-        List<Label> labelResultList = new List<Label>();
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            lblSpecial.Text = lblG1.Text = lblG2.Text = lblG3.Text
+                = lblG4.Text = lblG5.Text = lblG6.Text = lblG7.Text = " ";
         }
 
         private string guess(int number, int digit)
@@ -34,22 +33,56 @@ namespace GuessVietNamNorthenLottery
             {
                 int temp = random.Next(range);
                 string tmp = string.Format("{0,5:D5}", temp);
-                result += tmp.Substring(tmp.Length - digit) + ((number == 6 && i == 2) ? "\n" : "   "); 
+                result += tmp.Substring(tmp.Length - digit) + ((number == 6 && i == 2) ? "\n" : "     "); 
             }
 
             result = result.TrimEnd();
             return result;
         }
 
-        private string guess()
+        private void guess()
         {
-            double d = (DateTime.Now - dateTimePicker1.Value.Date).TotalDays;
-            return d.ToString();
+            lblSpecial.Text = guess(1, 5);
+            lblG1.Text = guess(1, 5);
+            lblG2.Text = guess(2, 5);
+            lblG3.Text = guess(6, 5);
+            lblG4.Text = guess(4, 4);
+            lblG5.Text = guess(6, 4);
+            lblG6.Text = guess(3, 3);
+            lblG7.Text = guess(4, 2);
+        }
+
+        private string guessCode()
+        {
+            DateTime ABCodeDay = DateTime.Parse("3/30/2021 6:9: 6AM",
+                          System.Globalization.CultureInfo.InvariantCulture);
+            List<string> code = new List<string>();
+            for(char i = 'A'; i<='Z'; i++)
+            {
+                for(char j = 'A'; i<='Z'; j++)
+                {
+
+                }
+            }
+            return "AB";
         }
 
         private void btnGuess_Click(object sender, EventArgs e)
         {
-            label11.Text = guess();
+            if((dateTimePicker1.Value.Date - DateTime.Today).TotalDays < 0)
+            {
+                MessageBox.Show("Ngày đã qua\nHãy chọn lại ngày");
+                return;
+            }else if ((dateTimePicker1.Value.Date - DateTime.Today).TotalDays == 0)
+            {
+                if(dateTimePicker1.Value.Hour > 18)
+                {
+                    MessageBox.Show("Giờ vàng đã qua\nHãy chọn ngày tới để dự đoán");
+                    return;
+                }
+            }
+
+            guess();
         }
     }
 }
